1ae3702d24c065de499acdb410243f34be97b9a0
ElvenLS <33652530+ElvenLS@users.noreply.github.com>
2018-05-03 11:19:02 +0700

    report
    yg tadi ak hapus ak jadikan komen

--- includes/header.php
+++ includes/header.php
@@ -30,7 +30,11 @@
 
   <!-- bootstrap js -->
 	<script src="assests/bootstrap/js/bootstrap.min.js"></script>
-
+<!-- 
+  <script
+  src="http://code.jquery.com/jquery-3.3.1.js"
+  integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+  crossorigin="anonymous"></script> -->
 
 </head>
 <body>
